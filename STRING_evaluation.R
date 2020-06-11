ec4e13e311329ba78da27a09f7f31974e06a5fff
javier-pardodiaz <55745072+javier-pardodiaz@users.noreply.github.com>
2020-06-11 12:37:12 +0100

    Create STRING_evaluation.R

---
+++ STRING_evaluation.R
@@ -0,0 +1,6 @@
+# Code to analyse the networks (previously saved) using STRING
+# It requieres 3 arguments:
+# 1) Name (including path) which will be used to load the files (it needs to be the same as in generate_sdcor_GCN.R)
+# 2) Path to STRING data with the proper format (see STRING_format.R)
+
+# This code saves and plots the results of the evaluation using STRING
